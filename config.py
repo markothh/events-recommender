b1794f5b3d039793352e12a50f2ca7f06e74309b
test-user <>
2026-04-29 10:02:36 +0000

    docker + vm

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
+import os
+import urllib.parse
+
+db_url = os.environ.get('DATABASE_URL', 'postgresql://postgres:123123@db:5432/events_db')
+parsed = urllib.parse.urlparse(db_url)
+
 DB = {
-    "host": "localhost",
-    "port": 5432,
-    "dbname": "recsys",
-    "user": "postgres",
-    "password": "123123"
+    "host": parsed.hostname or "db",
+    "port": parsed.port or 5432,
+    "dbname": parsed.path.lstrip('/') if parsed.path else "events_db",
+    "user": parsed.username or "postgres",
+    "password": parsed.password or "123123"
 }
 
-KUDAGO_API = "https://kudago.com/public-api/v1.4"
-YANDEX_API = "31329796-880c-441b-b0e1-2b6fc0b9cdea"
+KUDAGO_API = os.environ.get('KUDAGO_API', 'https://kudago.com/public-api/v1.4')
+YANDEX_API = os.environ.get('YANDEX_API', '31329796-880c-441b-b0e1-2b6fc0b9cdea')
+
+SECRET = os.environ.get('SECRET', 'tarasova_sofya_iuk4-82b_programm_engineering_diploma')
+
 
 SIMILARITY_CONTRIBUTION = 0.75
 CITY_CHANGE_PENALTY = 0.3
@@ -18,8 +27,3 @@ LEARNING_RATE_WATCH = 0.01
 DECAY_RATE = 0.01
 NEW_TAG_INIT_WATCH = 0.2
 NEW_TAG_INIT_GRADE = 0.4
-
-
-
-SECRET = "ddhdfhsdfhsofhsofhsofhsofhofhsofhsofhsofhsufhdijnf"
-
